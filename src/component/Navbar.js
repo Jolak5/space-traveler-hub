40758698be58c382560ecb03cc795f69f174a81b
MuhammadUsmanAslam <musmanaslam520@gmail.com>
2023-04-10 18:49:33 +0500

    Add navbar design

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,18 +1,20 @@
+import './Navbar.css';
 import React from 'react';
 import { NavLink, Outlet } from 'react-router-dom';
+import logo from '../assets/planet.png';
 
 const Navbar = () => (
   <div>
     <header>
-      <nav>
-        <div>
-          <h1>Speace Travelers`&apos;` Hub</h1>
-        </div>
-        <div>
-          <NavLink to="/">Missions</NavLink>
-          <NavLink to="rockets">Rockets</NavLink>
-          <NavLink to="myprofile">My Profile</NavLink>
-        </div>
+      <a className="logo" href="/">
+        <img src={logo} alt="Logo" />
+        <h1>Speace Travelers&apos; Hub</h1>
+      </a>
+      <nav className="navbar">
+        <NavLink to="/">Missions</NavLink>
+        <NavLink to="rockets">Rockets</NavLink>
+        <hr />
+        <NavLink to="myprofile">My Profile</NavLink>
       </nav>
     </header>
     <main>
