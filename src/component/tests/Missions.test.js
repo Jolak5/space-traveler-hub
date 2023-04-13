b654b8712351fc3cfca673f9a7160df9f9b6eabe
MuhammadUsmanAslam <musmanaslam520@gmail.com>
2023-04-13 13:33:10 +0500

    Add tests for remaining components

--- src/component/tests/Missions.test.js
+++ src/component/tests/Missions.test.js
@@ -3,6 +3,7 @@ import { cleanup, render } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import store from '../redux/store';
 import Missions from '../missions/Missions';
+import { getMissions, joinMission } from '../redux/missions/missionsSlice';
 
 afterEach(cleanup);
 
@@ -10,5 +11,16 @@ describe('Missions', () => {
   it('Missions to match Snapshot', () => {
     const quotes = render(<Provider store={store}><Missions /></Provider>);
     expect(quotes).toMatchSnapshot();
+  });
+});
+
+describe('Missions reducers', () => {
+  it('should fetch missions', async () => {
+    await store.dispatch(getMissions());
+    expect(store.getState().missions.missions.length).toEqual(10);
+  });
+  it('should join a mission', () => {
+    store.dispatch(joinMission('9D1B7E0'));
+    expect(store.getState().missions.missions[0].joined).toBe(true);
   });
 });
--- src/component/tests/Rockets.test.js
+++ src/component/tests/Rockets.test.js
@@ -2,13 +2,25 @@ import React from 'react';
 import { cleanup, render } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import store from '../redux/store';
-import Rocket from '../rockets/Rocket';
+import Rockets from '../rockets/Rockets';
+import { getRockets, changeReservedState } from '../redux/rockets/rocketsSlice';
 
 afterEach(cleanup);
 
-describe('Rockets', () => {
+describe('Rocket', () => {
   it('Rockets to match Snapshot', () => {
-    const quotes = render(<Provider store={store}><Rocket /></Provider>);
-    expect(quotes).toMatchSnapshot();
+    const rockets = render(<Provider store={store}><Rockets /></Provider>);
+    expect(rockets).toMatchSnapshot();
+  });
+});
+
+describe('Rockets reducers', () => {
+  it('should fetch rockets', async () => {
+    await store.dispatch(getRockets());
+    expect(store.getState().rockets.rockets.length).toEqual(4);
+  });
+  it('should reserve a rocket', () => {
+    store.dispatch(changeReservedState('9D1B7E0'));
+    expect(store.getState().rockets.rockets[0].reserved).toBe(false);
   });
 });
